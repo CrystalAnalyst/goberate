f844ce5645fb98027cab5b2928453ec5696744f4
kevin <kevin91cooland@gmail.com>
2023-11-21 16:47:42 +0800

    add Container Instance

--- framekwork/container.go
+++ framekwork/container.go
@@ -1,5 +1,10 @@
 package framework
 
+import (
+	"fmt"
+	"sync"
+)
+
 type Container interface {
 	Bind(comer ServiceProvider) error
 	Is_bind(key string) bool
@@ -7,3 +12,31 @@ type Container interface {
 	MustGetService(key string) interface{}
 	GetServiceByParams(key string, params []any) (interface{}, error)
 }
+
+// 服务容器实例
+type GobeContainer struct {
+	Container
+	providers map[string]ServiceProvider
+	instances map[string]any
+	lock      sync.RWMutex
+}
+
+func NewGobeContainer() *GobeContainer {
+	return &GobeContainer{
+		providers: map[string]ServiceProvider{},
+		instances: map[string]any{},
+		lock:      sync.RWMutex{},
+	}
+}
+
+func (gobe *GobeContainer) GetProviderList() []string {
+	ret := []string{}
+	for _, provider := range gobe.providers {
+		name := provider.Name()
+		line := fmt.Sprintf(name)
+		ret = append(ret, line)
+	}
+	return ret
+}
+
+/*----为GobeContainer实现Container接口----*/
