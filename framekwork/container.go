b9d7c5337c5fbbf99ddbcb6efa10e9b5dae2562d
kevin <kevin91cooland@gmail.com>
2023-11-21 20:08:38 +0800

    ready for impl container interface.

--- framekwork/container.go
+++ framekwork/container.go
@@ -1,6 +1,7 @@
 package framework
 
 import (
+	"errors"
 	"fmt"
 	"sync"
 )
@@ -45,6 +46,7 @@ func (gobe *GobeContainer) Bind(comer ServiceProvider) error {
 	name := comer.Name()
 	gobe.providers[name] = comer
 	gobe.lock.Unlock()
+
 	if comer.Is_defer() == false {
 		// 注册即实例化(必须返回一个可用的服务实例).
 		// 而服务实例需要先调用Boot完成参数、配置的初始化.
@@ -55,9 +57,28 @@ func (gobe *GobeContainer) Bind(comer ServiceProvider) error {
 		newInstanceFunc := comer.Register(gobe)
 		inst, err := newInstanceFunc(param...)
 		if err != nil {
-			// 实例化失败.
+			fmt.Println("bind failure! key: {}, error: {}!", name, err)
+			return errors.New(err.Error())
 		}
 		gobe.instances[name] = inst
 	}
 	return nil
 }
+
+func (gobe *GobeContainer) Is_bind(key string) bool {
+	if gobe.providers[key] != nil {
+		return true
+	} else {
+		return false
+	}
+}
+
+func (gobe *GobeContainer) GetService(key string) (interface{}, error) {
+	/*--在服务容器中用key(代表着服务名)获取对应的服务--*/
+}
+
+func (gobe *GobeContainer) MustGetService(key string) interface{
+	/*----*/
+}
+
+func (gobe *GobeContainer) GetServiceByParams(key string, params []any) (interface{}, error)
